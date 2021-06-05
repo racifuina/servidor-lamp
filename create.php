f75923b07d704f7b54f77f690b824e80fa4e9431
Rigoberto Acifuina <racifuina@me.com>
2021-06-05 17:47:22 -0600

    empleados

--- create.php
+++ create.php
@@ -42,7 +42,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Records created successfully. Redirect to landing page
-                header("location: index.php");
+                header("location: empleados.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
